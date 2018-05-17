618fbcb467b11d9c0d453a33d09f3caed72b84a0
adiWieli <dewa17a@gmail.com>
2018-05-18 05:55:23 +0700

    xx

---
+++ resources/views/master/vendor/tambah.blade.php
@@ -0,0 +1,146 @@
+<!-- Modal -->
+<div id="tambah-vendor" class="modal fade" role="dialog">
+  <div class="modal-dialog modal-lg">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header bg-gradient-info">
+        <h4 class="modal-title">Form Master Data Vendor</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <label>Vendor Name</label>
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <div class="form-group">
+              <input type="text" class="form-control form-control-sm" name="">
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <label>Vendor ID</label>
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <div class="form-group">
+              <input type="text" class="form-control form-control-sm" name="" readonly="">
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <label>Hometown</label>
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <div class="form-group">
+              <select class="form-control form-control-sm">
+                
+              </select>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <label>Phone Number</label>
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <div class="form-group">
+              <input type="text" class="form-control form-control-sm" name="">
+            </div>
+          </div>
+           
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <label>E-mail</label>
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <div class="form-group">
+              <input type="email" class="form-control form-control-sm" name="">
+            </div>
+          </div>
+
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <label>Birth Date</label>
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <div class="form-group">
+              <input type="text" maxlength="10" class="form-control form-control-sm datepicker" name="">
+            </div>
+          </div>
+          
+           <div class="col-md-3 col-sm-3 col-xs-12">
+            <label>Address</label>
+          </div>
+          <div class="col-md-9 col-sm-9 col-xs-12">
+            <div class="form-group">
+              <textarea class="form-control form-control-sm"></textarea>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <label>Vendor Type</label>
+          </div>
+          <div class="col-md-9 col-sm-9 col-xs-12">
+            <div class="form-group">
+              <select class="form-control form-control-sm">
+                <option>Piutang</option>
+                <option>Tunai</option>
+              </select>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <label>Plafon Credit</label>
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <div class="form-group">
+              <input type="text" class="form-control" name="">
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <label>Credit Terms</label>
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <div class="form-group">
+              <input type="text" class="form-control" name="">
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <label>Bank Name</label>
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <div class="form-group">
+              <input type="text" class="form-control form-control-sm" name="">
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <label>Account Number</label>
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <div class="form-group">
+              <input type="text" class="form-control form-control-sm" name="">
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <label>NPWP</label>
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <div class="form-group">
+              <input type="text" class="form-control" name="">
+            </div>
+          </div>
+          <div class="col-md-6 col-sm-0 col-xs-0">
+            <!-- Empty -->
+          </div>
+          <div class="col-md-3 col-sm-3 col-xs-12">
+            <label>Information</label>
+          </div>
+          <div class="col-md-9 col-sm-9 col-xs-12">
+            <div class="form-group">
+              <textarea class="form-control form-control-sm"></textarea>
+            </div>
+          </div>
+         </div>
+      </div>
+      <div class="modal-footer">
+        <button class="btn btn-primary" type="button">Save Data</button>
+        <button type="button" class="btn btn-warning" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
