f01d7a82f8688cea1407669134f960500fc8ebf9
AzrieL/Muttq <denyprasetyo41@gmail.com>
2018-05-25 16:34:48 +0700

    d

--- backupan/deny-tyy.sql
+++ backupan/deny-tyy.sql
@@ -24,9 +24,8 @@ CREATE TABLE IF NOT EXISTS `d_marketing` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table bisnis_atonergi.d_marketing: ~1 rows (approximately)
-DELETE FROM `d_marketing`;
 /*!40000 ALTER TABLE `d_marketing` DISABLE KEYS */;
-INSERT INTO `d_marketing` (`mk_id`, `mk_code`, `mk_name`, `mk_phone`, `mk_address`, `mk_email`, `mk_information`, `mk_type`) VALUES
+REPLACE INTO `d_marketing` (`mk_id`, `mk_code`, `mk_name`, `mk_phone`, `mk_address`, `mk_email`, `mk_information`, `mk_type`) VALUES
 	(1, 'MKT/00001', '1', '1', '1', '1', '1', 'sales');
 /*!40000 ALTER TABLE `d_marketing` ENABLE KEYS */;
 
@@ -43,9 +42,8 @@ CREATE TABLE IF NOT EXISTS `d_npenawaran` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table bisnis_atonergi.d_npenawaran: ~2 rows (approximately)
-DELETE FROM `d_npenawaran`;
 /*!40000 ALTER TABLE `d_npenawaran` DISABLE KEYS */;
-INSERT INTO `d_npenawaran` (`np_kode`, `np_id`, `np_kodeitem`, `np_marketing`, `np_price`, `np_lowerlimit`, `np_insert`, `np_update`) VALUES
+REPLACE INTO `d_npenawaran` (`np_kode`, `np_id`, `np_kodeitem`, `np_marketing`, `np_price`, `np_lowerlimit`, `np_insert`, `np_update`) VALUES
 	('NPN/00006', 6, 'BRG/2', 'MKT/00001', 300000.00, 1000000.00, '2018-05-24 12:14:47', '2018-05-24 02:13:31'),
 	('NPN/00009', 9, 'BRG/2', 'MKT/00001', 300000.00, 123123123.00, '2018-05-24 01:04:08', NULL);
 /*!40000 ALTER TABLE `d_npenawaran` ENABLE KEYS */;
@@ -72,9 +70,8 @@ CREATE TABLE IF NOT EXISTS `m_customer` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table bisnis_atonergi.m_customer: ~6 rows (approximately)
-DELETE FROM `m_customer`;
 /*!40000 ALTER TABLE `m_customer` DISABLE KEYS */;
-INSERT INTO `m_customer` (`c_id`, `c_code`, `c_name`, `c_hometown`, `c_birthday`, `c_phone`, `c_address`, `c_email`, `c_type`, `c_creditterms`, `c_plafon`, `c_bankname`, `c_npwp`, `c_accountnumber`, `c_information`, `c_insert`, `c_update`) VALUES
+REPLACE INTO `m_customer` (`c_id`, `c_code`, `c_name`, `c_hometown`, `c_birthday`, `c_phone`, `c_address`, `c_email`, `c_type`, `c_creditterms`, `c_plafon`, `c_bankname`, `c_npwp`, `c_accountnumber`, `c_information`, `c_insert`, `c_update`) VALUES
 	(1, 'MKT/00001', '11212312', 'spb', '2018-05-24', '1', '1', '1', 'Tunai', 1, 1, '1', '1', 1, '1', '2018-05-24 07:32:00', '2018-05-24 07:50:35'),
 	(2, 'MKT/00002', '2', 'spb', '2018-05-24', '2', '2', '2', 'Piutang', 2, 2, '2', '2', 2, '2', '2018-05-24 07:35:54', NULL),
 	(3, 'MKT/00003', NULL, NULL, '2018-05-24', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-05-24 10:20:12', NULL),
@@ -94,9 +91,8 @@ CREATE TABLE IF NOT EXISTS `m_item_bundling` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table bisnis_atonergi.m_item_bundling: ~5 rows (approximately)
-DELETE FROM `m_item_bundling`;
 /*!40000 ALTER TABLE `m_item_bundling` DISABLE KEYS */;
-INSERT INTO `m_item_bundling` (`ib_item`, `ib_detailid`, `ib_price`, `ib_isactive`, `ib_insert`, `ib_update`) VALUES
+REPLACE INTO `m_item_bundling` (`ib_item`, `ib_detailid`, `ib_price`, `ib_isactive`, `ib_insert`, `ib_update`) VALUES
 	('BARANG CABLE', 1, 77710000, NULL, '2018-05-25 08:09:02', NULL),
 	('BARANG CABLE', 2, 30600000, NULL, '2018-05-25 09:43:18', NULL),
 	('BARANG CABLE', 3, 110000, NULL, '2018-05-25 10:37:58', NULL),
@@ -117,9 +113,8 @@ CREATE TABLE IF NOT EXISTS `m_item_bundling_dt` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table bisnis_atonergi.m_item_bundling_dt: ~8 rows (approximately)
-DELETE FROM `m_item_bundling_dt`;
 /*!40000 ALTER TABLE `m_item_bundling_dt` DISABLE KEYS */;
-INSERT INTO `m_item_bundling_dt` (`ibd_id`, `ibd_detailid`, `ibd_barang`, `ibd_qty`, `ibd_unit`, `ibd_price`, `ibd_insert`, `ibd_update`) VALUES
+REPLACE INTO `m_item_bundling_dt` (`ibd_id`, `ibd_detailid`, `ibd_barang`, `ibd_qty`, `ibd_unit`, `ibd_price`, `ibd_insert`, `ibd_update`) VALUES
 	(1, '1', 'BRG/3', 1111, NULL, 11110000.00, '2018-05-25 08:09:02', NULL),
 	(1, '2', 'BRG/2', 222, NULL, 66600000.00, '2018-05-25 08:09:02', NULL),
 	(2, '1', 'BRG/2', 100, '1', 30000000.00, '2018-05-25 09:43:18', NULL),
@@ -145,9 +140,8 @@ CREATE TABLE IF NOT EXISTS `m_pegawai` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table bisnis_atonergi.m_pegawai: ~1 rows (approximately)
-DELETE FROM `m_pegawai`;
 /*!40000 ALTER TABLE `m_pegawai` DISABLE KEYS */;
-INSERT INTO `m_pegawai` (`mp_id`, `mp_kode`, `mp_nik`, `mp_name`, `mp_email`, `mp_address`, `mp_position`, `mp_status`, `mp_insert`, `mp_update`) VALUES
+REPLACE INTO `m_pegawai` (`mp_id`, `mp_kode`, `mp_nik`, `mp_name`, `mp_email`, `mp_address`, `mp_position`, `mp_status`, `mp_insert`, `mp_update`) VALUES
 	(2, 'PGW/00002', 1, '1', '1', '1', 'Magang', 'Sudah Menikah', '2018-05-24 11:01:21', '2018-05-24 11:15:22');
 /*!40000 ALTER TABLE `m_pegawai` ENABLE KEYS */;
 
@@ -175,9 +169,8 @@ CREATE TABLE IF NOT EXISTS `m_vendor` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table bisnis_atonergi.m_vendor: ~1 rows (approximately)
-DELETE FROM `m_vendor`;
 /*!40000 ALTER TABLE `m_vendor` DISABLE KEYS */;
-INSERT INTO `m_vendor` (`s_id`, `s_kode`, `s_company`, `s_name`, `s_address`, `s_email`, `s_phone`, `s_fax`, `s_termin`, `s_limit`, `s_npwp`, `s_information`, `s_accountnumber`, `s_bankname`, `s_insert`, `s_update`, `s_date`, `s_hometown`, `s_type`) VALUES
+REPLACE INTO `m_vendor` (`s_id`, `s_kode`, `s_company`, `s_name`, `s_address`, `s_email`, `s_phone`, `s_fax`, `s_termin`, `s_limit`, `s_npwp`, `s_information`, `s_accountnumber`, `s_bankname`, `s_insert`, `s_update`, `s_date`, `s_hometown`, `s_type`) VALUES
 	(3, 'VDR/00003', 'PT ALAMRAYA', 'ASEP HIDAYAT', 'jl alamsssss', 'ASEP@GMAIL.COM', '0999999999', NULL, '999999', 1000000, 1123123, 'gggggggggggggggggg', 91912301, 'BRI', '2018-05-25 01:33:56', NULL, '2018-05-24', 'malang', 'Piutang');
 /*!40000 ALTER TABLE `m_vendor` ENABLE KEYS */;
 
