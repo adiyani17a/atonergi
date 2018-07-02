70d966a813c22938a59120b5ec6e8bdef1a472dc
foxbloodiron <ariakbar6944@gmail.com>
2018-07-02 16:38:02 +0700

    tambah script cookies expires

--- resources/views/layouts/_script.blade.php
+++ resources/views/layouts/_script.blade.php
@@ -185,15 +185,13 @@ else {
 $('#sidebar-light-theme').on('click', function(){
     $('body').addClass('sidebar-light');
     $('#sidebar-light-theme').addClass('sidebar-light selected');
-    var cookie = Cookies.set('sidebar', 'sidebar-light');
-    console.log(cookie);
+    Cookies.set('sidebar', 'sidebar-light',{ expires : 365});
 
 });
 
 $('#sidebar-default-theme').on('click', function(){
     $('body').removeClass('sidebar-light');
-    var cookie = Cookies.set('sidebar', 'sidebar-default');
-    console.log(cookie);
+    Cookies.set('sidebar', 'sidebar-default',{ expires : 365});
 });
 
 // Get Cookie
@@ -238,35 +236,35 @@ else {
 // Navbar Option Cookie
 $('div.tiles.primary').on('click', function(){
     $('.navbar').addClass('navbar-primary');
-    Cookies.set('navbar', 'navbar-primary');
+    Cookies.set('navbar', 'navbar-primary', {expires : 365});
 });
 $('div.tiles.success').on('click', function(){
     $('.navbar').addClass('navbar-success');
-    Cookies.set('navbar', 'navbar-success');
+    Cookies.set('navbar', 'navbar-success', {expires : 365});
 });
 $('div.tiles.warning').on('click', function(){
     $('.navbar').addClass('navbar-warning');
-    Cookies.set('navbar', 'navbar-warning');
+    Cookies.set('navbar', 'navbar-warning', {expires : 365});
 });
 $('div.tiles.danger').on('click', function(){
     $('.navbar').addClass('navbar-danger');
-    Cookies.set('navbar', 'navbar-danger');
+    Cookies.set('navbar', 'navbar-danger', {expires : 365});
 });
 $('div.tiles.pink').on('click', function(){
     $('.navbar').addClass('navbar-pink');
-    Cookies.set('navbar', 'navbar-pink');
+    Cookies.set('navbar', 'navbar-pink', {expires : 365});
 });
 $('div.tiles.info').on('click', function(){
     $('.navbar').addClass('navbar-info');
-    Cookies.set('navbar', 'navbar-info');
+    Cookies.set('navbar', 'navbar-info', {expires : 365});
 });
 $('div.tiles.dark').on('click', function(){
     $('.navbar').addClass('navbar-dark');
-    Cookies.set('navbar', 'navbar-dark');
+    Cookies.set('navbar', 'navbar-dark', {expires : 365});
 });
 $('div.tiles.light').on('click', function(){
     $('.navbar').addClass('navbar-light');
-    Cookies.set('navbar', 'navbar-light');
+    Cookies.set('navbar', 'navbar-light', {expires : 365});
 });
 
 // Filter Search Menu Submenu Sidebar
