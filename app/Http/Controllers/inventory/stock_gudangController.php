da71ef1730ae9ac9bb4913e35d86137513c6008b
FvorY <ferdyp73@gmail.com>
2018-11-12 13:57:49 +0700

    .

--- app/Http/Controllers/inventory/stock_gudangController.php
+++ app/Http/Controllers/inventory/stock_gudangController.php
@@ -8,7 +8,7 @@ use Yajra\Datatables\Datatables;
 use DB;
 class stock_gudangController extends Controller
 {
- 	
+
 	 public function stockgudang()
 	 {
 	 	$po = DB::table('d_purchaseorder')->where('po_status','=','F')->get();
@@ -16,19 +16,22 @@ class stock_gudangController extends Controller
 	 }
 	 public function datatable_stockgudang()
 	 {
-      $list = DB::select("SELECT * from i_stock_gudang left join m_item on m_item.i_code = i_stock_gudang.sg_iditem");
+      $list = DB::table('i_stock_gudang')
+                ->leftjoin('m_item', 'i_code', '=', 'sg_iditem')
+                ->get();
+
           // return $list;
       $data = collect($list);
 
-      // for ($i=0; $i <count($data) ; $i++) { 
+      // for ($i=0; $i <count($data) ; $i++) {
       // 	$check_data_seq = DB::table('d_penerimaan_barang_dt')->where('pbdt_code','=',$data[$i]->pb_code)->get();
       // }
-      
+
       // return $check_data_seq;
       // return $data;
 
       return Datatables::of($data)
-        
+
               ->addColumn('detail', function ($data) {
                         return '<button data-toggle="modal" onclick="detail(this)"  class="btn btn-outline-primary btn-sm">Detail</button>';
               })
