6f1f73d8f3d32ab78f518ab4f2d5ae427aca7053
FvorY <ferdyp73@gmail.com>
2018-11-13 17:12:24 +0700

    .

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -61,8 +61,8 @@ class InventoryController extends Controller
         return view('inventory/barangkeluar/kartu_stok', compact('finalkode', 'item', 'mutasi'));
     }
     public function simpankartu(Request $request){
-      // DB::beginTransaction();
-      // try {
+      DB::beginTransaction();
+      try {
 
       $checkstock = DB::table('i_stock_gudang')
                       ->where('sg_iditem', $request->pbd_item)
@@ -117,7 +117,7 @@ class InventoryController extends Controller
                 ->where('i_stock_gudang.sg_iditem','=', $request->pbd_item)
                 ->where(DB::raw('(sm_qty - sm_use)'), '>', 0)
                 ->get();
-                
+
             $permintaan = $request->pbd_qty;
 
             DB::table('i_stock_gudang')
@@ -193,16 +193,16 @@ class InventoryController extends Controller
         }
       }
 
-      //   DB::commit();
-      //   return response()->json([
-      //     'status' => 'berhasil'
-      //   ]);
-      // } catch (\Exception $e) {
-      //   DB::rollback();
-      //   return response()->json([
-      //     'status' => 'gagal'
-      //   ]);
-      // }
+        DB::commit();
+        return response()->json([
+          'status' => 'berhasil'
+        ]);
+      } catch (\Exception $e) {
+        DB::rollback();
+        return response()->json([
+          'status' => 'gagal'
+        ]);
+      }
 
     }
     public function print_kartu_stok(Request $request)
